d2fe55fed6a8bb450853808e698f58bfaf9ff9a6
vbhavsar16 <imvivek1695@gmail.com>
2023-12-20 18:52:51 -0500

    Changed index files and added few more

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Example JavaScript function for interactivity
+function greetUser() {
+    var userName = prompt('Enter your name:');
+    alert('Hello, ' + userName + '! Welcome to the website.');
+}
+

649202538ff9b1c087d222923bba9da67b5621c8
vbhavsar16 <imvivek1695@gmail.com>
2024-04-19 03:54:28 -0400

    Re-imagined

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-
-// Example JavaScript function for interactivity
-function greetUser() {
-    var userName = prompt('Enter your name:');
-    alert('Hello, ' + userName + '! Welcome to the website.');
-}
-
